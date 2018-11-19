7f5eae388854592e1890a1383570fa0c5710690b
zanderlx <lsaplan97@gmail.com>
2018-11-18 23:22:25 -0800

    organized code, started user management

---
+++ Authorization.Test/AuthorizationTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Authorization.Test
+{
+    [TestClass]
+    public class AuthorizationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ UserManagement/UserService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagement
+{
+    public class UserService
+    {
+        private IUserRepository _repository;
+
+        // Constructor
+        public UserService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // Create User
+        public bool CreateUser(User user)
+        {
+            // Convert username (email) to all lowercase
+            // This is to prevent same email regardless of letter case
+            string name = user.Username.ToLower();
+            user.Username = name;
+            return _repository.CreateUser(user);
+        }
+
+        // Read/Get User
+        public User GetUser(string username)
+        {
+            return _repository.GetUser(username);
+        }
+
+        // Update User
+        public User UpdateUser(string username)
+        {
+            return _repository.UpdateUser(username);
+        }
+
+        // Delete User
+        public bool DeleteUser(string username)
+        {
+            return _repository.DeleteUser(username);
+        }
+
+        // TODO: Create more methods that a user uses
+        // OLD METHODS - COMMENTED BECAUSE WANTED TO JUST HAVE CRUD OPS
+        // Uncomment these if you want to use them
+        /*
+        public void UpdatePassword(string username, string password)
+        {
+            var user = _userRepository.GetUser(username);
+            user.Password = password;
+
+            _userRepository.UpdateUser(user);
+        }
+
+        public void UpdateCity(string username, string city)
+        {
+            var user = _userRepository.GetUser(username);
+            user.City = city;
+
+            _userRepository.UpdateUser(user);
+        }
+        */
+    }
+}
