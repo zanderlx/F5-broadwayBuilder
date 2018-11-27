0ec67950d9f86ed7483e36e1ad2c684611d04c61
Abi Castro <abixcastro@gmail.com>
2018-11-27 00:05:25 -0800

    Created Enums and Models folder

--- ServiceLayer.Test/AuthorizationTests.cs
+++ ServiceLayer.Test/AuthorizationTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using ServiceLayer.Enums;
+using ServiceLayer.Models;
 
 namespace ServiceLayer.Test
 {
--- ServiceLayer/Authorization.cs
+++ ServiceLayer/Authorization.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using ServiceLayer.Enums;
+using ServiceLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace ServiceLayer
 {
