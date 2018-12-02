50d32867813e49c118d4b451493acf81e2268b29
Abi Castro <abixcastro@gmail.com>
2018-12-01 19:09:14 -0800

    Added The Data Access Project: It includes a UserRepository, the DBContext,and the IUserRepository.
    I also fixed the UserServiceTest class to do integration tests for the CreateUser method.

---
+++ DataAccessLayer/BroadwayBuilderContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DataAccessLayer
+{
+    public class BroadwayBuilderContext : DbContext
+    {
+        public BroadwayBuilderContext(): base("name=BroadwayBuilder")
+        {
+
+        }
+
+        //Creating property on db context
+        //Allows to access table in db
+        public DbSet<UserEntity> Users { get; set; }
+    }
+}
