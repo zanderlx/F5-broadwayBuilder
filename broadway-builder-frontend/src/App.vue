abec535b3417bcfcea36bcd10e26205022651cda
FryedBryce <38364300+FryedBryce@users.noreply.github.com>
2019-03-04 01:40:02 -0800

    Added bulma and sass loader

--- broadway-builder-frontend/src/App.vue
+++ broadway-builder-frontend/src/App.vue
@@ -8,13 +8,8 @@
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+@import '../node_modules/bulma/bulma.sass'
+
+
 </style>
