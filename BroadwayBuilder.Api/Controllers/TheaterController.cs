6c65887890fcf172b83ff6c2ce5367f9f2038ce0
ohfoeshow <36217477+ohfoeshow@users.noreply.github.com>
2019-03-17 23:29:54 -0700

    Recloning to address changes in BroadwayBuilder.Api/BroadwayBuilder.Api.csproj that I can't change manually.

--- BroadwayBuilder.Api/Controllers/TheaterController.cs
+++ BroadwayBuilder.Api/Controllers/TheaterController.cs
@@ -1,15 +1,42 @@
-﻿using System;
+﻿using DataAccessLayer;
+using ServiceLayer.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using DataAccessLayer;
 
 namespace BroadwayBuilder.Api.Controllers
 {
     public class TheaterController : ApiController
     {
-        public Theater GetTheaterByID(Guid id) { }
+        [Route("theater/createtheater")]
+        [HttpPost]
+
+        public IHttpActionResult CreateTheater([FromBody] Theater theater)
+        {
+
+            using (var dbcontext = new BroadwayBuilderContext())
+            {
+                var theaterService = new TheaterService(dbcontext);
+
+                try
+                {
+                    theaterService.CreateTheater(theater);
+                    dbcontext.SaveChanges();
+
+                    return Content((HttpStatusCode)201,"Theater Created");
+
+                }
+                // Todo: add proper error handling
+                catch (Exception e)
+                {
+                    return BadRequest();
+                }
+
+            }
+
+        }
     }
 }
