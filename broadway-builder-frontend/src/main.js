5164fb8a0e04c0de222c6e25b6ad917f94602a43
zanderlx <lsaplan@gmail.com>
2019-03-23 11:49:17 -0700

    Added FontAwesomeIcons

--- broadway-builder-frontend/src/main.js
+++ broadway-builder-frontend/src/main.js
@@ -1,6 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faCloudUploadAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faTimes);
+library.add(faCloudUploadAlt);
+
+Vue.component("FontAwesomeIcon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
