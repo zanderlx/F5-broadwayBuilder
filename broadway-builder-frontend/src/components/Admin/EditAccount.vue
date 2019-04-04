21af54b13ad6683dfb28711bf83b5bc7f6e8cdfb
FryedBryce <38364300+FryedBryce@users.noreply.github.com>
2019-04-04 13:01:25 -0700

    Updated EditAccount Feature into Component

---
+++ broadway-builder-frontend/src/components/Admin/EditAccount.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="column is-6">
+    <div class="field">
+      <label class="label">Company Name</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Previous Info">
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Theater Name</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Previous Info">
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Phone Number</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="###-###-####">
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">Street</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Previous Info">
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">State</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Previous Info">
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">Country</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Previous Info">
+      </div>
+    </div>
+
+    <div class="field is-grouped">
+      <div class="control">
+        <button class="button is-link">Submit</button>
+      </div>
+      <div class="control">
+        <button class="button is-text">Cancel</button>
+      </div>
+    </div>
+  </div>
+</template>
