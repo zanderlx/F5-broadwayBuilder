3e988ed74d64a9dcdbe77c126554cb3b4f2d0c45
DeletedSilence <31982323+DeletedSilence@users.noreply.github.com>
2019-04-05 16:10:15 -0700

    added migrations and Jobtype property to TheaterJobPosting

---
+++ DataAccessLayer/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DataAccessLayer.BroadwayBuilderContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "DataAccessLayer.BroadwayBuilderContext";
+        }
+
+        protected override void Seed(DataAccessLayer.BroadwayBuilderContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
